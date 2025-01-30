08a8537c4a0bc13545644b103b0d3f05aa78ee81
OxymoRonald <1240635+OxymoRonald@users.noreply.github.com>
2025-01-30 15:51:35 +0100

    Added +1s

--- assets/dispatch_v2.js
+++ assets/dispatch_v2.js
@@ -1,6 +1,7 @@
 // ToDo
 // Add nesting for +1\
 // Re-order functions
+// If a child is removed from parent , remove from parents child list
 
 
 // JSON file name
@@ -54,6 +55,8 @@ function getStaff(){
                 // Set status and timestamp
                 staff_member.status = 42;
                 staff_member.timestamp = 0;
+                staff_member.children = [];
+                staff_member.parent = -1;
                 ems_staff.push(staff_member);
                 console.log("    " + staff_member.callsign + ": " + staff_member.name);
 
@@ -107,6 +110,8 @@ function updatePage(){
                 ems_staff_list_42 += "<div draggable='true' ondragstart='drag(event)' id='staff_" + index + "' class='staff_card'>"
                 ems_staff_list_42 += "<span class='no-drag staff_card_callsign'>"+ staff_member.callsign +"</span>"
                 ems_staff_list_42 += "<span class='no-drag'>"+ staff_member.name +"</span>"
+                ems_staff_list_42 += "<span class='no-drag'> (i="+ index +")</span>"
+                ems_staff_list_42 += "<span class='no-drag'> (" + staff_member.status + ")</span>"
                 ems_staff_list_42 += "</div>"  
 
             }
@@ -116,8 +121,23 @@ function updatePage(){
                 ems_staff_list_06 += "<div draggable='true' ondragstart='drag(event)' id='staff_" + index + "' class='staff_card'>"
                 ems_staff_list_06 += "<span class='no-drag staff_card_callsign'>"+ staff_member.callsign +"</span>"
                 ems_staff_list_06 += "<span class='no-drag'>"+ staff_member.name +"</span>"
+                ems_staff_list_06 += "<span class='no-drag'> (i="+ index +")</span>"
+                ems_staff_list_06 += "<span class='no-drag'> (" + staff_member.status + ")</span>"
                 ems_staff_list_06 += "<span class='no-drag'> (" + time_in_queue + ")</span>"
+                for(child in staff_member.children){
+                    // console.log("Child ID: " + child)
+                    // console.log("Child ID value: " + staff_member.children[child])
+
+                    ems_staff_list_06 += "<div draggable='true' ondragstart='drag(event)' id='staff_" + staff_member.children[child] + "' class='staff_card no-drag'>"
+                    ems_staff_list_06 += "<span class='no-drag staff_card_callsign'>"+ ems_staff[staff_member.children[child]].callsign +"</span>"
+                    ems_staff_list_06 += "<span class='no-drag'>"+ ems_staff[staff_member.children[child]].name +"</span>"
+                    ems_staff_list_06 += "<span class='no-drag'> (" + ems_staff[staff_member.children[child]].status + ")</span>"
+                    ems_staff_list_06 += "<span class='no-drag'> (i="+ index +")</span>"
+                    ems_staff_list_06 += "<span class='no-drag'> Parent: ("+ ems_staff[staff_member.children[child]].parent +")</span>"
+                    ems_staff_list_06 += "</div>"  
+                }
                 ems_staff_list_06 += "</div>"  
+
 
             }
             // Elif 10-7
@@ -126,17 +146,50 @@ function updatePage(){
                 ems_staff_list_07 += "<div draggable='true' ondragstart='drag(event)' id='staff_" + index + "' class='staff_card'>"
                 ems_staff_list_07 += "<span class='no-drag staff_card_callsign'>"+ staff_member.callsign +"</span>"
                 ems_staff_list_07 += "<span class='no-drag'>"+ staff_member.name +"</span>"
+                ems_staff_list_07 += "<span class='no-drag'> (i="+ index +")</span>"
+                ems_staff_list_07 += "<span class='no-drag'> (" + staff_member.status + ")</span>"
                 ems_staff_list_07 += "<span class='no-drag'> (" + time_in_queue + ")</span>"
+                for(child in staff_member.children){
+                    // console.log("Child ID: " + child)
+                    // console.log("Child ID value: " + staff_member.children[child])
+
+                    ems_staff_list_07 += "<div draggable='true' ondragstart='drag(event)' id='staff_" + staff_member.children[child] + "' class='staff_card no-drag'>"
+                    ems_staff_list_07 += "<span class='no-drag staff_card_callsign'>"+ ems_staff[staff_member.children[child]].callsign +"</span>"
+                    ems_staff_list_07 += "<span class='no-drag'>"+ ems_staff[staff_member.children[child]].name +"</span>"
+                    ems_staff_list_07 += "<span class='no-drag'> (" + ems_staff[staff_member.children[child]].status + ")</span>"
+                    ems_staff_list_07 += "<span class='no-drag'> (i="+ index +")</span>"
+                    ems_staff_list_07 += "<span class='no-drag'> Parent: ("+ ems_staff[staff_member.children[child]].parent +")</span>"
+                    ems_staff_list_07 += "</div>"  
+                }
                 ems_staff_list_07 += "</div>"  
+
 
             }
             // Elif 10-8
             else if(staff_member.status == 8){
+                console.log("Name: " + staff_member.name)
+                console.log("Children: " + staff_member.children)
                 
                 ems_staff_list_08 += "<div draggable='true' ondragstart='drag(event)' id='staff_" + index + "' class='staff_card'>"
                 ems_staff_list_08 += "<span class='no-drag staff_card_callsign'>"+ staff_member.callsign +"</span>"
                 ems_staff_list_08 += "<span class='no-drag'>"+ staff_member.name +"</span>"
+                ems_staff_list_08 += "<span class='no-drag'> (i="+ index +")</span>"
+                ems_staff_list_08 += "<span class='no-drag'> (" + staff_member.status + ")</span>"
                 ems_staff_list_08 += "<span class='no-drag'> (" + time_in_queue + ")</span>"
+                ems_staff_list_08 += "<span class='no-drag'> Children: ("+ staff_member.children +")</span>"
+                ems_staff_list_08 += "<span class='no-drag'> Parent: ("+ staff_member.parent +")</span>"
+                for(child in staff_member.children){
+                    // console.log("Child ID: " + child)
+                    // console.log("Child ID value: " + staff_member.children[child])
+
+                    ems_staff_list_08 += "<div draggable='true' ondragstart='drag(event)' id='staff_" + staff_member.children[child] + "' class='staff_card no-drag'>"
+                    ems_staff_list_08 += "<span class='no-drag staff_card_callsign'>"+ ems_staff[staff_member.children[child]].callsign +"</span>"
+                    ems_staff_list_08 += "<span class='no-drag'>"+ ems_staff[staff_member.children[child]].name +"</span>"
+                    ems_staff_list_08 += "<span class='no-drag'> (" + ems_staff[staff_member.children[child]].status + ")</span>"
+                    ems_staff_list_08 += "<span class='no-drag'> (i="+ index +")</span>"
+                    ems_staff_list_08 += "<span class='no-drag'> Parent: ("+ ems_staff[staff_member.children[child]].parent +")</span>"
+                    ems_staff_list_08 += "</div>"  
+                }
                 ems_staff_list_08 += "</div>"  
 
             }
@@ -146,8 +199,23 @@ function updatePage(){
                 ems_staff_list_47 += "<div draggable='true' ondragstart='drag(event)' id='staff_" + index + "' class='staff_card'>"
                 ems_staff_list_47 += "<span class='no-drag staff_card_callsign'>"+ staff_member.callsign +"</span>"
                 ems_staff_list_47 += "<span class='no-drag'>"+ staff_member.name +"</span>"
+                ems_staff_list_47 += "<span class='no-drag'> (i="+ index +")</span>"
+                ems_staff_list_47 += "<span class='no-drag'> (" + staff_member.status + ")</span>"
                 ems_staff_list_47 += "<span class='no-drag'> (" + time_in_queue + ")</span>"
+                for(child in staff_member.children){
+                    // console.log("Child ID: " + child)
+                    // console.log("Child ID value: " + staff_member.children[child])
+
+                    ems_staff_list_47 += "<div draggable='true' ondragstart='drag(event)' id='staff_" + staff_member.children[child] + "' class='staff_card no-drag'>"
+                    ems_staff_list_47 += "<span class='no-drag staff_card_callsign'>"+ ems_staff[staff_member.children[child]].callsign +"</span>"
+                    ems_staff_list_47 += "<span class='no-drag'>"+ ems_staff[staff_member.children[child]].name +"</span>"
+                    ems_staff_list_47 += "<span class='no-drag'> (" + ems_staff[staff_member.children[child]].status + ")</span>"
+                    ems_staff_list_47 += "<span class='no-drag'> (i="+ index +")</span>"
+                    ems_staff_list_47 += "<span class='no-drag'> Parent: ("+ ems_staff[staff_member.children[child]].parent +")</span>"
+                    ems_staff_list_47 += "</div>"  
+                }
                 ems_staff_list_47 += "</div>"  
+
 
             }
             // Else +1, add to parent
@@ -156,6 +224,8 @@ function updatePage(){
                 ems_staff_list_99 += "<div draggable='true' ondragstart='drag(event)' id='staff_" + index + "' class='staff_card'>"
                 ems_staff_list_99 += "<span class='no-drag staff_card_callsign'>"+ staff_member.callsign +"</span>"
                 ems_staff_list_99 += "<span class='no-drag'>"+ staff_member.name +"</span>"
+                ems_staff_list_99 += "<span class='no-drag'> (i="+ index +")</span>"
+                ems_staff_list_99 += "<span class='no-drag'> (" + staff_member.status + ")</span>"
                 ems_staff_list_99 += "<span class='no-drag'> (" + time_in_queue + ")</span>"
                 ems_staff_list_99 += "</div>"  
 
@@ -164,7 +234,7 @@ function updatePage(){
         }
 
         // Update page areas
-        console.log("    Updating HTML areas")
+        // console.log("    Updating HTML areas")
         document.getElementById("col_1006").innerHTML = ems_staff_list_06;
         document.getElementById("col_1007").innerHTML = ems_staff_list_07;
         document.getElementById("col_1008").innerHTML = ems_staff_list_08;
@@ -178,20 +248,59 @@ function updatePage(){
 
     load_end = Date.now();
     load_time = Math.round(load_start - load_end);
-    console.log("--- Updated page in " + load_time + " milliseconds ---");
+    // console.log("--- Updated page in " + load_time + " milliseconds ---");
     console.log(" ")
 };
 
 // On update (button push)
 function updateDispatch(id, status){
 
-    console.log("Update Dispatch ID: " + id)
-    console.log("Update Dispatch Status: " + status)
+    // console.log("Update Dispatch ID: " + id)
+    // console.log("Update Dispatch Status: " + status)
 
     // updateDispatch((updating).slice(6)*1, (ev.target.id).slice(6)*1)
     id = id.slice(6)*1
-    status = status.slice(6)*1
+    if(status.startsWith("col_")){
+        status = status.slice(6)*1
 
+        // If parent is set
+        if(ems_staff[id]['parent'] >= 0){
+            // Print parent ID
+            console.log("Parent ID: " + ems_staff[id]['parent']);
+
+            // Remove child from parent
+            // Get index of child ID
+            console.log("Parent Child List: " + ems_staff[parent_id].children)
+            console.log("Remove from parent: " + id)
+            console.log("")
+
+            const childindex = ems_staff[parent_id].children.indexOf(id);
+            console.log("Index: " + childindex)
+            if (childindex > -1) { // only splice array when item is found
+                ems_staff[parent_id].children.splice(childindex, 1); // 2nd parameter means remove one item only
+            }
+
+            // Reset parent of child
+            // ems_staff[id]['parent'] = -1;
+        }
+
+    }
+    else if(status.startsWith("staff_")){
+        // // console.log("Update Dispatch Status CHILD: " + status)
+        // child_callsign = ems_staff[id].callsign
+        // // console.log("Update Dispatch Child Callsign: " + child_callsign)
+        // parent_id = status.slice(6)*1
+        // ems_staff[parent_id].children.push(child_callsign)
+
+        
+        parent_id = status.slice(6)*1
+        ems_staff[parent_id].children.push(id)
+        
+        // Update parent ID of child
+        ems_staff[id]['parent'] = parent_id;
+
+
+    }
     // Update status for staff member
     ems_staff[id]['status'] = status;
 
@@ -204,7 +313,7 @@ function updateDispatch(id, status){
     }
 
     // Sort by timestamp, then callsign
-    ems_staff.sort((a,b) => a.timestamp - b.timestamp || a.callsign - b.callsign);
+    // ems_staff.sort((a,b) => a.timestamp - b.timestamp || a.callsign - b.callsign);
 
     // Update page
     updatePage();
@@ -235,13 +344,13 @@ function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
-    console.log("Target ID: " + ev.target.id)
-    console.log("Updating Child: " +  updating)
-    console.log("Child ID: " + (updating).slice(6))
-    console.log("Child Name: " + ems_staff[(updating).slice(6)].name)
+    // console.log("Target ID: " + ev.target.id)
+    // console.log("Updating Child: " +  updating)
+    // console.log("Child ID: " + (updating).slice(6))
+    // console.log("Child Name: " + ems_staff[(updating).slice(6)].name)
     // Update status for staff member
-    console.log("Old Status: " + ems_staff[(updating).slice(6)]['status'])
-    console.log("New Status: " + (ev.target.id).slice(6)*1)
+    // console.log("Old Status: " + ems_staff[(updating).slice(6)]['status'])
+    // console.log("New Status: " + (ev.target.id).slice(6)*1)
     // ems_staff[(updating).slice(6)]['status'] = (ev.target.id).slice(6);
     // updatePage();
     updateDispatch(updating, ev.target.id)
@@ -249,7 +358,7 @@ function drop(ev) {
 }
 
 
-// Update page sections every 30 seconds
+// Update page sections every 10 seconds
 setInterval(function(){ 
     console.log(timestamp() + " - Update page.")
     updatePage(); 
